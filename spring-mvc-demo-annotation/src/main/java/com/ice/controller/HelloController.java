a852a6cbc8b7ea1525e9390261c9841ae5eb2f16
dreaming-coder <644476114@qq.com>
2022-07-12 21:52:58 +0800

    新增 Spring MVC 注解版 demo

---
+++ spring-mvc-demo-annotation/src/main/java/com/ice/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.ice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    //真实访问地址 : localhost:8081/hello
+    @RequestMapping("/hello")
+    public String sayHello(Model model){
+        //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
+        model.addAttribute("msg","hello,SpringMVC Annotation!");
+        //web-inf/jsp/hello.jsp
+        return "hello";
+    }
+
+}
